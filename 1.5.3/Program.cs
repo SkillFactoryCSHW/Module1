63474a45de8a1c750b3897fb83479cfa356db6dc
Bochanikita <bochanikita@yandex.ru>
2022-07-16 16:54:26 +0300

    Add task 1.5.3

---
+++ 1.5.3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Как Вас зовут?");
+string name = Console.ReadLine();
+Console.WriteLine("Сколько Вам лет?");
+string age = Console.ReadLine();
+Console.WriteLine("Откуда вы?");
+string city = Console.ReadLine();
+
+Console.WriteLine("Здравствуйте, {0}", name);
+Console.WriteLine("Вам {0} лет", age);
+Console.WriteLine("Вы из города {0}", city);
