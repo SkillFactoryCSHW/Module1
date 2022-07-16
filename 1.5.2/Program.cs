b387619430b13f986fd9c0269b4a3242c970d9ad
Bochanikita <bochanikita@yandex.ru>
2022-07-16 16:46:32 +0300

    Add task 1.5.2

---
+++ 1.5.2/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Как Вас зовут?");
+string name = Console.ReadLine();
+Console.WriteLine("Здравствуйте, {0}", name);
